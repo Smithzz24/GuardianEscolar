14117b5f28435e1ec4b1d7da6c4fa52f2c27f4ee
JuanP <jpchalaramirez@gmail.com>
2026-03-25 21:11:34 -0500

    feat(main): Implementation of the content of the page body

--- dvlp-front/dvlp-web/src/app/app.ts
+++ dvlp-front/dvlp-web/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuLandingPage } from './menu-landing-page/menu-landing-page';
+import { Main } from './main/main';
 
 @Component({
   selector: 'app-root',
-  imports: [MenuLandingPage, RouterOutlet],
+  imports: [MenuLandingPage, RouterOutlet, Main],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
