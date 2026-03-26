b6cd0fac7f10d90bf5f424fc1df417a101d13859
Sharik <sharikrojas.sena@gmail.com>
2026-03-26 17:42:39 -0500

    feat: implementation of the admin dashboard

--- dvlp-front/dvlp-web/src/app/app.routes.ts
+++ dvlp-front/dvlp-web/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/login/login';
 import { Home } from './features/home/home';
+import { DashboardAdmin } from './features/dashboard-admin/dashboard-admin';
 
 export const routes: Routes = [
-    { path: "", component: Home},
-    { path: "login", component: Login}
+    { path: "", component: Home },
+    { path: "login", component: Login },
+    { path: "dashboard-admin", component: DashboardAdmin }
 ];
