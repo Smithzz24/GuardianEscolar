e5f6ef053fe8bb892a7580fd8be57af8d36e90d4
JuanP <jpchalaramirez@gmail.com>
2026-03-26 15:52:21 -0500

    style(login): Creation of the login page along with the management of url paths

--- dvlp-front/dvlp-web/src/app/app.ts
+++ dvlp-front/dvlp-web/src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Navbar } from './shared/navbar/navbar';
-import { Main } from './shared/main/main';
-import { Footer } from './shared/footer/footer';
+import { Home } from './features/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, RouterOutlet, Main, Footer],
+  imports: [Home, RouterOutlet,],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
