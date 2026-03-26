3fbc12661773b7a58830ac98147b0214c7ad8356
JuanP <jpchalaramirez@gmail.com>
2026-03-25 21:42:15 -0500

    feat(footer): Implementation of footer content

--- dvlp-front/dvlp-web/src/app/app.ts
+++ dvlp-front/dvlp-web/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuLandingPage } from './menu-landing-page/menu-landing-page';
 import { Main } from './main/main';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [MenuLandingPage, RouterOutlet, Main],
+  imports: [MenuLandingPage, RouterOutlet, Main, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
