5dac79eb5923d30e36693a9aa5fc9d14e89c3d8c
JuanP <jpchalaramirez@gmail.com>
2026-03-26 15:52:21 -0500

    style(login): Creation of the login page along with the management of url paths

---
+++ dvlp-front/dvlp-web/src/app/features/login/login.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Footer } from '../../shared/footer/footer';
+import { NavComponent } from '../../shared/nav-component/nav-component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-login',
+  imports: [Footer, NavComponent, MatIconModule, MatButtonModule, MatToolbarModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {}
