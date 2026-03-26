76556423a984924f52e6cb08666e3e9d6fb108f3
JuanP <jpchalaramirez@gmail.com>
2026-03-26 13:39:20 -0500

    chore: dependency correction

--- dvlp-front/dvlp-web/src/app/app.ts
+++ dvlp-front/dvlp-web/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuLandingPage } from './shared/navbar/navbar';
+import { Navbar } from './shared/navbar/navbar';
 import { Main } from './shared/main/main';
 import { Footer } from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [MenuLandingPage, RouterOutlet, Main, Footer],
+  imports: [Navbar, RouterOutlet, Main, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
