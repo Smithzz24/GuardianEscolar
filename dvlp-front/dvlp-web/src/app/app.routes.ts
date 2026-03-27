26c4fe62729bdd20d7fab2623e271f440ed283b1
JuanP <jpchalaramirez@gmail.com>
2026-03-26 22:45:48 -0500

    style(email, code, reset): creation of pages to reset password

--- dvlp-front/dvlp-web/src/app/app.routes.ts
+++ dvlp-front/dvlp-web/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
-import { Login } from './features/login/login';
+import { Login } from './features/auth/login/login';
 import { Home } from './features/home/home';
+import { ForgotPassword } from './features/auth/forgot-password/forgot-password';
+import { routes as forgotPasswordRoutes } from './features/auth/forgot-password/forgot-password.routes'
 
 export const routes: Routes = [
     { path: "", component: Home},
-    { path: "login", component: Login}
+    { path: "auth/login", component: Login},
+    { path: "auth/forgot-password", component: ForgotPassword, children: forgotPasswordRoutes}
 ];
