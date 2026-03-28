21c797292a944686466d61066a506d2f6d3cd19c
Sharik <sharikrojas.sena@gmail.com>
2026-03-28 16:10:53 -0500

    style: Improved icons and added color

--- dvlp-front/dvlp-web/src/app/features/dashboard-admin/dashboard-admin.ts
+++ dvlp-front/dvlp-web/src/app/features/dashboard-admin/dashboard-admin.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NavbarDashboard } from '../../shared/navbar-dashboard/navbar-dashboard';
 
 @Component({
   selector: 'app-dashboard-admin',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  standalone: true, 
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    NavbarDashboard 
+  ],
   templateUrl: './dashboard-admin.html',
-  styleUrl: './dashboard-admin.css',
+  styleUrls: ['./dashboard-admin.css'],
 })
 export class DashboardAdmin {}
