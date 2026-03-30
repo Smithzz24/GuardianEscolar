4098a2ab74669af91b2c38ff4cc43d5ded129fdf
Sharik <sharikrojas.sena@gmail.com>
2026-03-30 16:28:03 -0500

    feat: Creating admin and superadmin administration navbar

---
+++ dvlp-front/dvlp-web/src/app/shared/components/navbar/navbar-manage/navbar-manage.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-manage',
+  imports: [],
+  templateUrl: './navbar-manage.html',
+  styleUrl: './navbar-manage.css',
+})
+export class NavbarManage {}
