1fd11729bc599d814003e1dfe9949428dc77f82f
JuanP <jpchalaramirez@gmail.com>
2026-04-01 14:51:22 -0500

    style(change-password): implementation of initial styles

---
+++ dvlp-front/dvlp-web/src/app/features/profile/pages/change-password/steps/reset/reset.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NavComponent } from "../../../../../../shared/components/navbar/nav-component/nav-component";
+
+@Component({
+  selector: 'app-reset',
+  imports: [NavComponent],
+  templateUrl: './reset.html',
+  styleUrl: './reset.css',
+})
+export class Reset {}
