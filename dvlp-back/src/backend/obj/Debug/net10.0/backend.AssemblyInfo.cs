3c1545b27bea5d3601bf049d3a1dd135a51a7635
Sharik <sharikrojas.sena@gmail.com>
2026-04-27 09:33:45 -0500

    feature: The theme change modal is now functional; only style adjustments are needed.

--- dvlp-back/src/backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ dvlp-back/src/backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a44d81c3d5d682165eae1094c8c518bfdab30b25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50b9d25d8a5d01fda41cceb83a37b4bc9f1b9fc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
